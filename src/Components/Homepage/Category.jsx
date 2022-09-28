6b968d45c75c5814b3063c2f04d4a34b28797ff6
Makinde Tobiloba <makinde.tobiloba@gmail.com>
2022-09-28 02:08:21 +0100

    blog-site

--- src/Components/Homepage/Category.jsx
+++ src/Components/Homepage/Category.jsx
@@ -1,11 +1,14 @@
-const Category = (props) => {
+import { useState } from "react";
+const Category = () => {
+    const [category, setCategory] = useState(["♟ Politics", "🎲 Games", "💱 Crypto", " 💻 Tech",
+        "⛪ Religion", "💰  Finances", "📚 Education", "🏀 Sport", "📱 Blogging", "🤡 Fictions", "⚙ Engineering"])
     return (
         <div className="d-flex flex-column p4">
-            <div className="w-100 shadow-sm p-1 mb-2"> <strong className="fs-5">Category:</strong></div>
+            <div className="w-100 shadow-sm p-1 px-2 bg-dark mb-2 rounded-1"> <strong className="fs-5 text-white">Category</strong></div>
 
-            <div className="d-flex  flex-column gap-2 p-2">
-                {props.items.map((data, index) =>
-                    <span className="bg-light shadow  btn-sm category" key={index}>{data}</span>
+            <div className="d-flex  flex-column gap-2 p-2 ">
+                {category.map((data, index) =>
+                    <span className="bg-light px-1 py-1 btn-sm category  rounded-2" key={index}>{data}</span>
                 )}
             </div>
         </div>
@@ -13,3 +16,5 @@ const Category = (props) => {
 }
 
 export default Category;
+
+
